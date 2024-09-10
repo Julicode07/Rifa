efa73e080c0b983662317d0ade459c759a7d722e
JuliCode07 <Julicode07@gmail.com>
2024-09-10 16:28:11 -0500

    First commit with clean code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+
+  return (
+    <>
+      <h1>Clean Code</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
